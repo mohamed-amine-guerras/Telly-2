c9a066896c7db8720f027c69333dffbf2945ff87
Oussama Bougaila <oussama.bougaila@gmail.com>
2018-11-12 11:30:07 +0100

    feat: Add createReserve methode CreateReserve will create a bus reservation

--- src/com/telly/controllers/BusController.java
+++ src/com/telly/controllers/BusController.java
@@ -30,6 +30,19 @@ public class BusController {
 		return "createtrip";
 	}
 
+	@RequestMapping(value = "/createreserve", method = RequestMethod.POST)
+	public String createReserve(@Validated(FormValidationGroup.class) Bus bus, BindingResult result,
+			Principal principal) {
+
+		if (result.hasErrors()) {
+			return "reservebus";
+		}
+
+		busService.create(bus);
+
+		return "home";
+
+	}
 
 	
 
