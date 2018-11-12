c8c2ddd90c8332d503983d2b14d9862603d34d84
mohamed-amine-guerras <fm_guerras@esi.dz>
2018-11-12 11:31:23 +0100

    feat: Add create user feature

--- src/com/telly/controllers/UserController.java
+++ src/com/telly/controllers/UserController.java
@@ -11,7 +11,9 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.telly.dao.FormValidationGroup;
 import com.telly.dao.User;
+import com.telly.service.UserService;
 
 
 @Controller
@@ -30,6 +32,22 @@ public class UserController {
 		model.addAttribute("user", new User());
 		
 		return "createaccount";
+	}
+
+	@RequestMapping(value = "/createuser", method = RequestMethod.POST)
+	public String createUser(@Validated(FormValidationGroup.class) User user, BindingResult result) {
+		
+		if(result.hasErrors()) {
+			return "createaccount";
+		}
+		
+		user.setAuthority("ROLE_USER");
+		user.setEnabled(true);
+
+		userService.create(user);
+		
+		return "home";
+
 	}
 	
 
